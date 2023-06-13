b751ddfb4bd024f8672bba0a22ac631c87dd647e
Cassia Martinelli <martinelli.cassia@gmail.com>
2023-06-13 08:35:56 -0300

    feat: add authentication to routes

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,22 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true, // Permite que qualquer endereço de frontend acesse este backend
 })
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
